61455a61b99f57ddeff6e0f1aea240b81d7d6aeb
काला बिच्छू 🦂 <jaiveer2003godara@gmail.com>
2025-08-26 12:37:33 +0530

    Delete modules directory

--- modules/vars.py
+++
@@ -1,7 +0,0 @@
-import os
-
-API_ID    = os.environ.get("API_ID", "20937734")
-API_HASH  = os.environ.get("API_HASH", "eeb2040e50b0532a3fd97f386cd6d131")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8370955321:AAFUjQAT_45i5PWecLMHPQH-e2gUenXogbQ") 
-WEBHOOK = True  # Don't change this
-PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
